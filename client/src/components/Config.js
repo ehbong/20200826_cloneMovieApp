a6151380084c20aeead7018616bd8101aa692a2e
ehbong <hangillw86@gmail.com>
2020-08-26 23:38:10 +0900

    Add LandingPage Main Image, Connect moviedb api

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'cc409e4fbdd045c7c84a2e38c6914c61';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL} from './../../Config';
+import MainImage from './Section/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovie, setMainMovie] = useState(null);
+    useEffect(() => {
+       //const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+       const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+
+        fetch(endpoint)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            setMovies(res.results);
+            setMainMovie(res.results[0]);
+        })
+
+    }, []);
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+       <div style={{ width: '100%', margin: '0' }}>
+           {/* Main image */}
+            {MainMovie && 
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovie.backdrop_path}`} 
+                    title={MainMovie.original_title}
+                    text={MainMovie.overview}
+                    />
+            }
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                    <h2>Movies by lastest</h2>
+                    <hr/>
+
+                    {/* Movie Grid Cards */}
+
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+       </div>
     )
 }
 
